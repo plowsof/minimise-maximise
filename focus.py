1b51e68ec9404da5d68728f00d1a7ba4e9c07689
d3nd3 <2523452+d3nd3@users.noreply.github.com>
2021-01-18 11:51:32 +0000

    fixed stackoverflows

--- focus.py
+++ focus.py
@@ -79,7 +79,7 @@ def callback(hWinEventHook, event, hwnd, idObject, idChild, dwEventThread,
             tup = win32gui.GetWindowPlacement(sofId)
             break
         except:
-            onSoFWindowHandleChange()
+            searchForSoFWindow()
 
     minimized = True
     if tup[1] == win32con.SW_SHOWMAXIMIZED:
@@ -182,7 +182,7 @@ def getRes(hwnd):
             rect = win32gui.GetWindowRect(hwnd)
             break
         except:
-            onSoFWindowHandleChange()
+            searchForSoFWindow()
     x = rect[0]
     y = rect[1]
     w = rect[2] - x
